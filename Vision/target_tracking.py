01f4e78036a67446981ce12ec9a8972b4e555084
Victor Song <vsong1618@gmail.com>
2018-02-28 20:14:42 -0600

    Began implementing target cost algorithm

--- Vision/target_tracking.py
+++ Vision/target_tracking.py
@@ -31,6 +31,8 @@ camera_matrix = np.array([[7.8041108095538573e+02, 0., 320.],
 camera_distortion_matrix = np.array([[1.2967497853950866e-01, 4.5607189281390443e+00, 0., 0., -2.4946392110455260e+01]])
 new_camera_matrix, region_of_interest = cv2.getOptimalNewCameraMatrix(camera_matrix, camera_distortion_matrix, (res_x, res_y), 1, (res_x, res_y))
 
+lowest_cost = 300
+
 def get_target_values(frame):
     clear = make_true_frame(frame)
     mask = erode_dilate_mask(clear)
@@ -73,8 +75,34 @@ def find_contoured_centroid(mask):
         center = ((center1[0] + center2[0]) / 2, (center1[1] + center2[1]) / 2)
 
         logging.info("Contours: %s", str.format(ordered_cnts))
+        
+        return center
+# TODO: implement target cost algorith 
+def find_optimal_contours(contours):
+    best_contours = None
+    for p1, p2 in combinations(contours, 2):
+        c1, a1 = p1[0], p1[1]
+        c2, a2 = p2[0], p2[1]
 
-    return center
+        cost = target_cost(c1, c2)
+        if cost < lowest_cost:
+            lowest_cost = cost
+            best_contours = (c1, c2)
+    return (c1, c2)
+
+def target_cost(c1, c2):
+    M1 = cv2.moments(c1)
+    cx1 = int(M1['m10'] / M1['m00'])
+    cy1 = int(M1['m01'] / M1['m00'])
+    M2 = cv2.moments(c2)
+    cx2 = int(M2['m10'] / M2['m00'])
+    cy2 = int(M2['m01'] / M2['m00'])
+
+    offset_x = abs(cx1 - cx2)
+    offset_y = abs(cy1 - cy2)
+
+    return offset_x * 6 + offset_y
+
 # TODO: use getStructuringElement in later iterations
 def erode_dilate_mask(frame):
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
